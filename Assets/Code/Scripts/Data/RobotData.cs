679bd8d70d9d76a34e5f9cd6cbc16477210c9fb2
EmilKhadiev <newemil00@mail.ru>
2026-08-18 13:04:09 +0300

    Add robot data

---
+++ Assets/Code/Scripts/Data/RobotData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/" + nameof(RobotData), fileName = nameof(RobotData))]
+public class RobotData : ScriptableObject
+{
+    [field: SerializeField] public float StartHealth { get; private set; }
+    [field: SerializeField] public float StartDamage { get; private set; }
+    [field: SerializeField] public float StartAttackSpeed { get; private set; }
+    [field: SerializeField] public float MoveSpeed { get; private set; }
+    [field: SerializeField] public float StartPrice { get; private set; }
+    public int RobotLevel = 1;
+}
